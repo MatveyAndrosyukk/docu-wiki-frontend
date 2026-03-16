0fded7b0ac4a06ecbd7445cf76046958c669ebf8
matveyhf <matveyhf@gmail.com>
2026-03-16 11:03:09 +0300

    refactor(login-modal): clean-code jsx

--- src/shared/ui/modal-windows/login-modal/LoginModal.tsx
+++ src/shared/ui/modal-windows/login-modal/LoginModal.tsx
@@ -1,14 +1,16 @@
-import React, {FC, useCallback} from 'react';
-import styles from './LoginModal.module.scss'
-import modalStyles from '../modal/ModalContent.module.scss'
+import React, { FC, useCallback } from 'react';
+import styles from './LoginModal.module.scss';
+import modalStyles from '../modal/ModalContent.module.scss';
 import Modal from "../modal/Modal";
 import GoogleButton from "../../google-button/GoogleButton";
-import {ReactComponent as SwitchAuthSvg} from './images/login-modal-switch-auth.svg';
-import {useAuth} from "../../../lib/hooks/useAuth";
-import {useAppContext} from "../../../lib/hooks/useAppContext";
+import { ReactComponent as SwitchAuthSvg } from './images/login-modal-switch-auth.svg';
+import { useAuth } from "../../../lib/hooks/useAuth";
+import { useAppContext } from "../../../lib/hooks/useAppContext";
 
 const LoginModal: FC = () => {
-    const {authState} = useAppContext();
+    const { authState } = useAppContext();
+    const { authStatus } = useAuth();
+
     const {
         isLoginModalOpen,
         handleCloseAuthModal,
@@ -30,14 +32,22 @@ const LoginModal: FC = () => {
         getAuthorizationText,
     } = authState;
 
-    const {authStatus} = useAuth();
-
     const handleOpenEnterEmailModal = useCallback(() => {
-        handleCloseAuthModal()
+        handleCloseAuthModal();
         setIsEnterEmailModalOpened(true);
-    }, [handleCloseAuthModal, setIsEnterEmailModalOpened])
+    }, [handleCloseAuthModal, setIsEnterEmailModalOpened]);
 
     if (!isLoginModalOpen) return null;
+
+    const messageText = loginMessage || registerMessage || loginError || registerError;
+    const messageClassName = loginMessage || registerMessage
+        ? modalStyles.modal__message
+        : loginError || registerError
+            ? modalStyles.modal__error
+            : `${modalStyles.modal__message} ${modalStyles.hidden}`;
+
+    const emailValue = isRegisterModal ? registerModalValue.email : loginModalValue.login;
+    const passwordValue = isRegisterModal ? registerModalValue.password : loginModalValue.password;
 
     return (
         <Modal
@@ -46,74 +56,73 @@ const LoginModal: FC = () => {
         >
             <div className={`${modalStyles['modal__overlay']} ${styles['login-modal__overlay']}`}>
                 <div className={`${modalStyles['modal__form']} ${styles['login-modal__form']}`}>
-                    <div className={`${styles['login-modal__header']}`}>
-                        <p className={`${styles['login-modal__form-text']}`}>
-                            {isRegisterModal ? 'Register' : 'Login'}</p>
+
+                    <div className={styles['login-modal__header']}>
+                        <p className={styles['login-modal__form-text']}>
+                            {isRegisterModal ? 'Register' : 'Login'}
+                        </p>
                         <SwitchAuthSvg
-                            className={`${styles['login-modal__switch']}`}
-                            onClick={handleSwitchAuthorization}/>
+                            className={styles['login-modal__switch']}
+                            onClick={handleSwitchAuthorization}
+                        />
                     </div>
-                    <div className={
-                        loginMessage || registerMessage
-                            ? modalStyles.modal__message
-                            : (loginError || registerError)
-                                ? modalStyles.modal__error
-                                : `${modalStyles.modal__message} ${modalStyles.hidden}`
-                    }>
-                        {loginMessage || registerMessage || loginError || registerError}</div>
+
+                    <p className={messageClassName}>{messageText}</p>
+
                     <input
                         ref={loginModalInputRef}
                         type='text'
-                        className={`${modalStyles['modal__input']}`}
-                        placeholder={"Enter your email"}
-                        value={isRegisterModal ? registerModalValue.email : loginModalValue.login}
-                        onChange={(e) => handleChangeEmailInput(e)}
+                        className={modalStyles['modal__input']}
+                        placeholder="Enter your email"
+                        value={emailValue}
+                        onChange={handleChangeEmailInput}
                     />
                     <input
                         type='password'
-                        className={`${modalStyles['modal__input']}`}
-                        placeholder={"Enter your password"}
-                        value={isRegisterModal ? registerModalValue.password : loginModalValue.password}
-                        onChange={(e) => handleChangePasswordInput(e)}
+                        className={modalStyles['modal__input']}
+                        placeholder="Enter your password"
+                        value={passwordValue}
+                        onChange={handleChangePasswordInput}
                     />
-                    {isRegisterModal &&
+
+                    {isRegisterModal && (
                         <input
                             type='password'
-                            className={`${modalStyles['modal__input']}`}
-                            placeholder={"Repeat your password"}
+                            className={modalStyles['modal__input']}
+                            placeholder="Repeat your password"
                             value={registerModalValue.rePassword}
-                            onChange={(e) => handleChangeRePasswordInput(e)}
+                            onChange={handleChangeRePasswordInput}
                         />
-                    }
-                    <div className={`${styles['login-modal__footer']}`}>
-                        <div className={`${styles['footer__left']}`}>
+                    )}
+
+                    <div className={styles['login-modal__footer']}>
+                        <div className={styles['footer__left']}>
                             <p
-                                className={`${styles['login-modal__forgot-password']}`}
-                                onClick={handleOpenEnterEmailModal}>
+                                className={styles['login-modal__forgot-password']}
+                                onClick={handleOpenEnterEmailModal}
+                            >
                                 Forgot password?
                             </p>
                         </div>
-                        <div className={`${styles['footer__center']}`}>
+
+                        <div className={styles['footer__center']}>
                             <button
-                                className={`${modalStyles.modal__button}`}
+                                className={modalStyles.modal__button}
                                 disabled={loginLoading || authStatus === 'loading'}
                                 onClick={handleAuthorize}
                             >
-                                {loginLoading ? (
-                                    <span className={styles['login-modal__loader']}/>
-                                ) : (
-                                    getAuthorizationText()
-                                )}
+                                {loginLoading ? <span className={styles['login-modal__loader']} /> : getAuthorizationText()}
                             </button>
                         </div>
-                        <div className={`${styles['footer__right']}`}>
-                            <GoogleButton/>
+
+                        <div className={styles['footer__right']}>
+                            <GoogleButton />
                         </div>
                     </div>
                 </div>
             </div>
         </Modal>
-    )
+    );
 };
 
 export default LoginModal;
