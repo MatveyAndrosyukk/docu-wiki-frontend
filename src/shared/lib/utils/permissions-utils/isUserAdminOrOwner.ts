a341d43d05962d1fba7779b0db058b8eb74f4b9f
matveyhf <matveyhf@gmail.com>
2026-03-23 13:43:06 +0300

    fix(isUserAdminOrOwner) checked only on admin

--- src/shared/lib/utils/permissions-utils/isUserAdminOrOwner.ts
+++ src/shared/lib/utils/permissions-utils/isUserAdminOrOwner.ts
@@ -3,7 +3,7 @@ import {User} from "../../../../store/slices/userSlice";
 export const isUserAdminOrOwner = (
     loggedInUser: User | null,
 ) => {
-    const isUserAdminOrOwner = loggedInUser?.roles.some(role => role.value === "ADMIN");
+    const isUserAdminOrOwner = loggedInUser?.roles.some(role => role.value === "ADMIN" || "OWNER");
 
     return !!isUserAdminOrOwner;
 
